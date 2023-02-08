3e427a110ed522d821235792ff6c4ff89a7bad29
Nguyễn Đức Quý <Quyndph26096@fpt.edu.vn>
2023-02-08 20:06:57 +0700

    update section 11

--- section_11/main.js
+++ section_11/main.js
@@ -99,3 +99,99 @@ currenciesUnique.forEach(function(value,key,map) {
 /*
     146. PROJECT "BANKIST" APP 
 */
+/*
+    148. Coding Chanllege #1
+*/
+/*
+const checkDogs = function(dogsJulia,dogsKate) {
+    const dogJuliaCorrected = dogsJulia.slice();
+    dogJuliaCorrected.splice(0, 1);
+    dogJuliaCorrected.splice(-1);
+    const dogs = [...dogJuliaCorrected,...dogsKate];
+    console.log(dogs);
+    dogs.forEach(function(dog, i) {
+        const checkDogs = (dog > 3 ) ? ` is an adult, and is ${dog} years old` : ` is still a puppy`;
+        console.log(`Dog number ${i + 1} ${checkDogs}`);
+    })
+
+}
+checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3]);
+*/
+/*
+    ========== 150. The Map Method ===============
+*/
+/*
+const eurToUsd = 1.1;
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movementsUSD =  movements.map(function(mov) {
+//     return mov * eurToUsd;
+// });
+const movementsUSD = movements.map(mov => mov * eurToUsd)
+console.log(movements);
+console.log(movementsUSD);
+const movementsUSDfor = [];
+for (const mov of movements) {
+    movementsUSDfor.push(mov * eurToUsd);
+}
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i , arr) => 
+    // if(mov > 0) {
+    //     return `Movement ${i + 1}: You deposited ${mov}`;
+    // } else {
+    //     return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+    // }
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+)
+console.log(movementsDescriptions);
+*/
+/*
+    ======= 152. The Filter Method ========
+*/
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function(mov) {
+    return mov > 0;
+})
+console.log(movements);
+console.log(deposits);
+const depositsFor = [];
+for(const mov of movements) {
+    if(mov > 0) {
+        depositsFor.push(mov);
+    }
+}
+console.log(depositsFor);
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+*/
+/*
+    ======= 153. The Reduce Method ========
+*/
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+// accumulator -> SNOWBALl
+const balance = movements.reduce(function(acc,cur, i, arr) {
+    console.log(`Interation ${i} : ${acc}`);
+    return  acc + cur;
+}, 0);
+console.log(balance);
+let balance2 = 0;
+for(const mov of movements) {
+    balance2 += mov;
+}
+console.log(balance);
+
+// Maximum value 
+const max = movements.reduce((acc, mov) => {
+    if(acc > mov) {
+        return acc;
+    } else {
+        return mov;
+    }
+}, movements[0]);
+console.log(max);
+
+
+
+
